b0ea63f5c68726a6a1c499da2f1cca5f7c79f730
Darien Hernandez <darienhz89@gmail.com>
2021-01-21 15:55:42 -0800

    added a switch name button that switches the state and switches the names on click

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
    // Dont do this!! this.state.persons[0].name = 'Darien Hernandez';
    this.setState({
      persons: [
-      { name: 'Darien', age: 31 },
-      { name: 'Justin', age: 31 },
-      { name: 'Joey', age: 32 },
+      { name: 'Darien Hernandez', age: 31 },
+      { name: 'Justin Pla', age: 31 },
+      { name: 'Joey G', age: 32 },
     ]
   })
 }
@@ -31,7 +31,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>Learning to code with React</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNamedHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My hobbies: Video games</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
