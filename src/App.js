37197c29aa11885e7792c0b5f4d9c575bde08cc8
Darien Hernandez <darienhz89@gmail.com>
2021-01-21 14:52:55 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person'
+
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I'm a React App</h1>
+        <p>Learning to code with React</p>
+      </div>
+
+
+    );
+  }
+}
+
+export default App;
