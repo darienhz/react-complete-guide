58858c73ba4a5185564c33bf2e17552f873acacd
Darien Hernandez <darienhz89@gmail.com>
2021-01-21 15:36:01 -0800

    added state to persons

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import Person from './Person/Person'
 
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Darien', age: 31 },
+      { name: 'Justin', age: 31 },
+      { name: 'Joey', age: 32 },
+    ]
 
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>Learning to code with React</p>
+        <button>Switch Name</button>
         <Person name="Darien" age="31"> My hobbies: Video games</Person>
         <Person name="Justin" age="31"/>
         <Person name="Joey" age="32"/>
       </div>
-
-
     );
   }
 }
