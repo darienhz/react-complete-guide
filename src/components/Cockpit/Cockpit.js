fbc3b34ba93866363141d987454f9686bd19022b
Darien Hernandez <darienhz89@gmail.com>
2021-01-26 13:22:01 -0800

    changed the titles of our person manager

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -18,7 +18,7 @@ const cockpit = (props) => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi I'm a React App</h1>
+      <h1>{props.title}</h1>
         <p className={assignedClasses.join(' ')}>Learning to code with React</p>
         <button className={btnClass}
         onClick={props.clicked}>Switch Name</button>
