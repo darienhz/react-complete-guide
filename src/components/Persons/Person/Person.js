7a6d404b9d7abb0ea818493516ff4760f7b3ccc4
Darien Hernandez <darienhz89@gmail.com>
2021-02-01 19:22:02 -0800

    added a HOC folder and used Aux instead of a div for testing

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
-    return [
+    return (
     // <div className="Person" style={style}>
     // <div className={classes.Person}>
-      <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>,
-      <p key="i2">{this.props.children}</p>,
-      <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-    // </div>
-    ];
+    <Aux>
+      <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+      <p>{this.props.children}</p>
+      <input type="text" onChange={this.props.changed} value={this.props.name}/>
+    </Aux>
+    );
    }
 }
 
