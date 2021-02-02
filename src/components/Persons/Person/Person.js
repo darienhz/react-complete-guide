804a95c0fbdee7deae2438f7de6a43f2b8a08f82
Darien Hernandez <darienhz89@gmail.com>
2021-02-02 13:18:53 -0800

    used ref to put focus on the last input element after rendering the page

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types'
 
 class Person extends Component {
+  componentDidMount() {
+    this.inputElement.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -12,11 +17,18 @@ class Person extends Component {
     <Aux>
       <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
       <p>{this.props.children}</p>
-      <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      <input type="text" onChange={this.props.changed} value={this.props.name}
+      ref={(inputEl) => {this.inputElement = inputEl}}/>
     </Aux>
     );
    }
 }
 
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
